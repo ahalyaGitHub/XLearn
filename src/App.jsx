89671bdb76a7db76088c5e4d67e01ab49bc3a27e
ahalya_r <127035624+Ahalyagithub@users.noreply.github.com>
2024-05-04 11:33:55 +0530

    Some changes made in homePage

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,25 @@ const App = () => {
     <>
       <div>
         <Nav />
-        
-        <div className="container-fluid bg-dark">
-          
-            <div className="container">
-              <div className="row">
-                <div className="col-6 pt-5">
-                  <img src="./src/images/home.png" alt="" width={500} height={500} />
-                </div>
-                <div className="col-6">
-                  <p className="text text-light text-center mt-5  fs-5">Provide guidance and support to students who are embarking on their web development journey. Xlearn
-                    is here to offer a structured path for getting started and mastering the basics of web development.
-                    We just suggest top tamil YouTube channels and to facilitate web page learning journey for the
-                    students who don’t know where to start their journey. </p>
-                </div>
-              </div>
+
+        <div className="container-fluid">
+
+
+          <div className="row">
+            <div className="col-md-6">
+              <img src="./src/images/home.png" alt="studyImg" className="img-fluid w-500"/>
             </div>
-       
+            <div className="col-md-6">
+              <p className="text text-light text-center mt-5 pt-5 fs-5">Provide guidance and support to students who are embarking on their web development journey. Xlearn
+                is here to offer a structured path for getting started and mastering the basics of web development.
+                We just suggest top tamil YouTube channels and to facilitate web page learning journey for the
+                students who don’t know where to start their journey. </p>
+                <button type="button" class="btn btn-outline-warning fw-bold">Start Learning</button>
+
+            </div>
+          </div>
         </div>
+
       </div>
     </>
   );
